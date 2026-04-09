8955ff2b4427d245adfd518d22cce124a074be4e
Sidd3115 <patilsiddhi3115@gmail.com>
2026-04-09 20:07:27 +0530

    Add files via upload

---
+++ singly.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+int data;
+struct node *next;
+};
+struct node *head = NULL;
+void insert(int x) 
+{
+struct node *newnode, *temp;
+newnode = (struct node*)malloc(sizeof(struct node));
+newnode->data = x;
+newnode->next = NULL;
+if(head == NULL) 
+{
+head = newnode;
+} else
+{
+temp = head;
+while(temp->next != NULL) 
+{
+temp = temp->next;
+}
+temp->next = newnode;
+}
+}
+void display() 
+{
+struct node *temp = head;
+while(temp != NULL) 
+{
+printf("%d -> ", temp->data);
+temp = temp->next;
+}
+printf("NULL\n");
+}
+int main() 
+{
+insert(10);
+insert(20);
+insert(30);
+display();
+return 0;
+}
