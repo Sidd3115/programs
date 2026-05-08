d449b36e3ada417cc0ff64ec255d28256b534fbc
Sidd3115 <patilsiddhi3115@gmail.com>
2026-05-08 11:00:37 +0530

    Add files via upload

---
+++ Internal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+int arr[100], n;
+cout << "Enter no.of elements: ";
+cin >> n;
+cout << "Enter array elements:\n";
+for(int i = 0; i < n; i++) 
+{
+cin >> arr[i];
+}
+for(int i=0; i<n-1; i++)
+{
+int min = i;
+for(int j=i + 1;j<n;j++) 
+{
+if(arr[j] < arr[min]) 
+{
+min = j;
+}
+}
+int temp = arr[i];
+arr[i] = arr[min];
+arr[min] = temp;
+}
+cout << "Sorted array:\n";
+for(int i=0; i<n;i++) 
+{
+cout << arr[i] << " ";
+}
+return 0;
+}
